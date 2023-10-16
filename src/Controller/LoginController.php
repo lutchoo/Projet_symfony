134170e2f8ebb0d117c0d966b97e41cedf96029a
lucas <lucasjulien.12100@gmail.com>
2023-10-16 15:59:09 +0200

    oublier d'enregister vs code

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -21,6 +21,7 @@ class LoginController extends AbstractController
             'error'         => $error,
         ]);
     }
+    
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout(): never
     {
